2ffd30111ca520ddff2bc0a5c1e6a68e72fbfd37
Robert Michalski <robertm1013@gmail.com>
2022-05-10 19:49:43 +0200

    Adding files
    Adding basic files from Spring initializer

---
+++ src/main/java/Carbuyer/demo/CarbuyerApplication.java
@@ -0,0 +1,13 @@
+package Carbuyer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbuyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbuyerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Carbuyer/demo/CarbuyerApplicationTests.java
@@ -0,0 +1,13 @@
+package Carbuyer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbuyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
