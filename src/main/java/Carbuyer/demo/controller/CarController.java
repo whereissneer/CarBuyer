64d2a140eae08a92d4d5cd7565a6073875fea8e7
Robert Michalski <robertm1013@gmail.com>
2022-05-15 19:00:15 +0200

    Login form
    Added simple login form

--- src/main/java/Carbuyer/demo/controller/CarController.java
+++ src/main/java/Carbuyer/demo/controller/CarController.java
@@ -8,16 +8,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import Carbuyer.demo.entity.Car;
+import Carbuyer.demo.entity.User;
 import Carbuyer.demo.service.CarService;
+import Carbuyer.demo.service.UserService;
 
 @Controller
 public class CarController {
 
 		private CarService carService;
-
-		public CarController(CarService carService) {
+		private UserService userService;
+		public CarController(CarService carService, UserService userService) {
 			super();
 			this.carService = carService;
+			this.userService = userService;
 		};
 		
 		@GetMapping("/api/cars")
@@ -25,6 +28,7 @@ public class CarController {
 			model.addAttribute("cars", carService.getAllCars());
 			return "cars";
 		}
+		
 		@GetMapping("/api/cars/new")
 		public String createNewCarOfferForm(Model model) {
 			Car car = new Car();
@@ -42,6 +46,12 @@ public class CarController {
 		public String viewOffer(@PathVariable Long id, Model model) {
 			model.addAttribute("car", carService.getCarById(id));
 			return "viewOffer";
+		}
+		@GetMapping("/api/cars/login")
+		public String loginForm(Model model) {
+			User user = new User();
+			model.addAttribute("user", user);
+			return "createNewUserForm";
 		}
 	
 }
